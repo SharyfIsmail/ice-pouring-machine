54baf8d0b92cb52fcbead22b78e73d535d302c61
SharyfIsmail <xandrey95x@live.ru>
2020-12-17 00:10:28 +0300

    corrected the dataFromDevice implementation for conactors and dataFromDeviceModel implementation for Contactors_0CFEF301_Model

--- app/src/main/java/com/oim/icepouring/batteryModule/model/Contactors_0CFEF301_Model.java
+++ app/src/main/java/com/oim/icepouring/batteryModule/model/Contactors_0CFEF301_Model.java
@@ -8,63 +8,36 @@ import com.oim.icepouring.batteryModule.tx.Contactors_0CFEF301;
 import com.oim.icepouring.can.candata.DataFromDevice;
 import com.oim.icepouring.can.candata.DataFromDeviceModel;
 
+import java.util.List;
 import java.util.Map;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public class Contactors_0CFEF301_Model implements DataFromDeviceModel {
 
     private Contactors_0CFEF301 contactors_0CFEF301;
-    private ObservableField<String> prechargeState;
-    private ObservableField<String> plusState;
-    private ObservableField<String> groundState;
-    private ObservableField<String> sensorState;
+    private List<String> errors;
 
-    public ObservableField<String> getPrechargeState() {
-        return prechargeState;
+    public List<String> getErrors() {
+        return errors;
     }
 
-    public void setPrechargeState(ObservableField<String> prechargeState) {
-        this.prechargeState = prechargeState;
-    }
-
-    public ObservableField<String> getPlusState() {
-        return plusState;
-    }
-
-    public void setPlusState(ObservableField<String> plusState) {
-        this.plusState = plusState;
-    }
-
-    public ObservableField<String> getGroundState() {
-        return groundState;
-    }
-
-    public void setGroundState(ObservableField<String> groundState) {
-        this.groundState = groundState;
-    }
-
-    public ObservableField<String> getSensorState() {
-        return sensorState;
-    }
-
-    public void setSensorState(ObservableField<String> sensorState) {
-        this.sensorState = sensorState;
+    public void setErrors(List<String> errors) {
+        this.errors = errors;
     }
 
     public Contactors_0CFEF301_Model()
     {
         contactors_0CFEF301 = new Contactors_0CFEF301();
-        prechargeState = new ObservableField<>();
-        plusState = new ObservableField<>();
-        groundState = new ObservableField<>();
-        sensorState = new ObservableField<>();
+        errors = new CopyOnWriteArrayList<>();
+
     }
 
     @Override
     public void updateModel() {
-        prechargeState.set(getErrorName(contactors_0CFEF301.getContactorError().getPrecharge()));
-        plusState.set(getErrorName(contactors_0CFEF301.getContactorError().getPlus()));
-        groundState.set(getErrorName(contactors_0CFEF301.getContactorError().getGround()));
-        sensorState.set(getErrorName(contactors_0CFEF301.getContactorError().getSensor()));
+      setGroundState(contactors_0CFEF301.getContactorState());
+      setPlusState(contactors_0CFEF301.getContactorState());
+      setPrechargeState(contactors_0CFEF301.getContactorState());
+      setSensorState(contactors_0CFEF301.getContactorState());
     }
 
     @Override
@@ -72,13 +45,85 @@ public class Contactors_0CFEF301_Model implements DataFromDeviceModel {
         return contactors_0CFEF301;
     }
 
-    private String getErrorName(Map<Integer, String> map)
+    private void setPrechargeState(short data)
     {
-        StringBuilder stringBuilder = new StringBuilder("");
-            for(Map.Entry<Integer,String> entry :map.entrySet())
-            {
-                stringBuilder.append(entry.getValue());
-            }
-            return stringBuilder.toString();
-     }
+        if((data & 1) == 1)
+        {
+            errors.add("Precharge OpenLoad");
+        }
+        else
+            errors.remove("Precharge OpenLoad");
+        if((data & 2) == 2)
+        {
+            errors.add("Precharge Welding");
+        }
+        else
+            errors.remove("Precharge Welding");
+
+        if((data & 4) == 4)
+        {
+            errors.add("Precharge feedback broken");
+        }
+        else
+            errors.remove( "Precharge feedback broken");
+    }
+
+    private  void setPlusState(short data)
+    {
+        if((data & 8) == 8)
+        {
+            errors.add( "Plus Open Load");
+        }
+        else
+            errors.remove("Plus Open Load");
+
+        if((data & 16) == 16)
+        {
+            errors.add("Plus Welding");
+        }
+        else
+            errors.remove("Plus Welding");
+        if((data  & 32) == 32)
+        {
+            errors.add("Plus relay feedback is broken");
+        }
+        else
+            errors.remove("Plus relay feedback is broken");
+    }
+    private  void setGroundState(short data)
+    {
+        if((data & 64) == 64)
+        {
+            errors.add( "Ground relay Open Load");
+        }
+        else
+            errors.remove("Ground relay Open Load");
+        if((data & 128) == 128)
+        {
+            errors.add("Ground relay Welding");
+        }
+        else
+            errors.remove("Ground relay Welding");
+        if((data  & 256) == 256)
+        {
+            errors.add("Ground relay feedback is broken");
+        }
+        else
+            errors.remove("Ground relay feedback is broken");
+    }
+    private  void setSensorState(short data)
+    {
+        if((data & 512)== 512)
+        {
+            errors.add("Voltage Sensor is Broken");
+        }
+        else
+            errors.remove("Voltage Sensor is Broken");
+        if((data  & 1024) == 1024)
+        {
+            errors.add("Current Sensor is Broken");
+        }
+        else
+            errors.remove("Current Sensor is Broken");
+    }
 }
--- app/src/main/java/com/oim/icepouring/batteryModule/tx/Contactors_0CFEF301.java
+++ app/src/main/java/com/oim/icepouring/batteryModule/tx/Contactors_0CFEF301.java
@@ -9,11 +9,8 @@ import java.util.Map;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 public class Contactors_0CFEF301 implements DataFromDevice {
-    private byte prechargeState ;
-    private byte plusState ;
-    private byte groundState ;
+
     private short contactorState;
-    private BatteryError batteryError = new BatteryError();
 
     public short getContactorState() {
         return contactorState;
@@ -23,173 +20,12 @@ public class Contactors_0CFEF301 implements DataFromDevice {
         this.contactorState = contactorState;
     }
 
-    public byte getPrechargeState() {
-        return prechargeState;
-    }
-
-    public void setPrechargeState(byte prechargeState) {
-        this.prechargeState = prechargeState;
-    }
-
-    public byte getPlusState() {
-        return plusState;
-    }
-
-    public void setPlusState(byte plusState) {
-        this.plusState = plusState;
-    }
-
-    public byte getGroundState() {
-        return groundState;
-    }
-
-    public void setGroundState(byte groundState) {
-        this.groundState = groundState;
-    }
-
-    public BatteryError getContactorError (){return batteryError ; }
-
     @Override
     public void parseDataFromCan(byte[] data) {
-        if(data.length == 8) {
-            prechargeState  = (byte) Parser.LittleIndianParser.uint_8ToShort(data[1]);
-            plusState = (byte) Parser.LittleIndianParser.uint_8ToShort(data[2]);
-            groundState = (byte) Parser.LittleIndianParser.uint_8ToShort(data[3]);
-
+        if(data.length == 8){
             byte[] partArray = new byte[2];
             System.arraycopy(data, 4, partArray, 0, partArray.length);
             contactorState = (short) Parser.LittleIndianParser.uint_16ToInt(partArray);
-            batteryError.setGroundState(contactorState);
-            batteryError.setPlusState(contactorState);
-            batteryError.setPrechargeState(contactorState);
-            batteryError.setSensorState(contactorState);
         }
-    }
-    public static class BatteryError
-    {
-        private List<Integer> errors = new CopyOnWriteArrayList<>();
-
-
-        private  Map<Integer, String> precharge = new HashMap<>();
-        private  Map<Integer, String> plus = new HashMap<>();
-        private  Map<Integer, String> ground = new HashMap<>();
-        private  Map<Integer, String> sensor = new HashMap<>();
-
-        public Map<Integer, String> getPrecharge() {
-            return precharge;
-        }
-
-        public Map<Integer, String> getPlus() {
-            return plus;
-        }
-
-        public Map<Integer, String> getGround() {
-            return ground;
-        }
-
-        public Map<Integer, String> getSensor() {
-            return sensor;
-        }
-
-        public void setErrorCodes(short data)
-        {
-
-            if((data & 1) == 1)
-            {
-                errors.add(1);
-            }
-            else
-                errors.remove(new Integer(1));
-            if((data & 2) == 2)
-            {
-                errors.add(2);
-            }
-            else
-                errors.remove(new Integer(2));
-        }
-
-        public  void setPrechargeState(short data)
-       {
-
-           if((data & 1) == 1)
-           {
-               precharge.put( 1, "Precharge Open Load");
-           }
-           else
-               precharge.remove( 1);
-
-          if((data & 2) == 2)
-           {
-               precharge.put(2, "Precharge Welding");
-           }
-          else
-              precharge.remove(2);
-
-            if((data & 4) == 4)
-            {
-               precharge.put(4, "Precharge relay feedback broken");
-            }
-            else
-                precharge.remove( 4);
-
-       }
-       public  void setPlusState(short data)
-       {
-           if((data & 8) == 8)
-           {
-               plus.put(8, "Plus Open Load");
-           }
-           else
-           plus.remove(8);
-
-           if((data & 16) == 16)
-           {
-               plus.put(16, "Plus Welding");
-           }
-           else
-              plus.remove(16);
-           if((data  & 32) == 32)
-           {
-               plus.put( 32, "Plus relay feedback broken");
-           }
-           else
-            plus.remove(32);
-       }
-       public  void setGroundState(short data)
-       {
-           if((data & 64) == 64)
-           {
-               ground.put( 64, "Ground Open Load");
-           }
-           else
-               ground.remove(64);
-           if((data & 128) == 128)
-           {
-               ground.put(128, "Ground Welding");
-           }
-           else
-               ground.remove(128);
-           if((data  & 256) == 256)
-           {
-               ground.put(256, "Ground relay feedback broken");
-           }
-           else
-               ground.remove(256);
-       }
-       public  void setSensorState(short data)
-       {
-           if((data & 512)== 512)
-           {
-               sensor.put( 512, "External Voltage Sensor Broken");
-           }
-           else
-               sensor.remove(512);
-           if((data  & 1024) == 1024)
-           {
-               sensor.put(1024, "Power Switch Current Sensor Crash");
-           }
-           else
-               sensor.remove(1024);
-       }
     }
 }
